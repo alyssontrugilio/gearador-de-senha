2f99f110baffe7a188b1c6f368e757cb4b084940
AlyssonTrugilio <alysson.trugilio.dev@gmail.com>
2025-01-24 17:05:22 -0300

    feat: adicionado input para o tamanho da senha

--- src/Container.jsx
+++ src/Container.jsx
@@ -6,17 +6,18 @@ export default function Container() {
 
   const [copyText, setCopyText] = useState('Copiar');
 
+  const [passwordSize, setPasswordSize] = useState(12);
+
   const copyPassword = () => {
     window.navigator.clipboard.writeText(password);
     setCopyText('Copiado!');
   };
 
   const generatePassword = () => {
-    const length = 12;
     const charset =
       'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
     let newPassword = '';
-    for (let i = 0, n = charset.length; i < length; ++i) {
+    for (let i = 0, n = charset.length; i < passwordSize; ++i) {
       newPassword += charset.charAt(Math.floor(Math.random() * n));
     }
     setCopyText('Copiar!');
@@ -25,8 +26,20 @@ export default function Container() {
   return (
     <div className={styles.container}>
       <h1>Gerador de senha</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={ev => setPasswordSize(ev.target.value)}
+        />
+      </div>
       <div className={styles.buttons}>
-        <button onClick={() => generatePassword()}>Gerar</button>
+        <button onClick={() => generatePassword()}>
+          Gerar senha de {passwordSize}
+        </button>
         <button onClick={() => copyPassword()}>{copyText}</button>
       </div>
       <p>{password}</p>
